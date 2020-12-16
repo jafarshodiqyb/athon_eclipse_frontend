8cf43cfbd7ea64bc1675d702fb8e97ca66ccffa8
Jafar <mendoandev@gmail.com>
2020-12-16 21:47:57 +0700

    alert redux

---
+++ src/reducers/alert.reducer.js
@@ -0,0 +1,20 @@
+import { alertTypes } from './../redux/alert.type';
+
+export function alert(state = {}, action) {
+  switch (action.type) {
+    case alertTypes.SUCCESS:
+      return {
+        type: 'alert-success',
+        message: action.message
+      };
+    case alertTypes.ERROR:
+      return {
+        type: 'alert-danger',
+        message: action.message
+      };
+    case alertTypes.CLEAR:
+      return {};
+    default:
+      return state
+  }
+}
