f75c52efe6f405ee804dfc9971d0f01d41227344
Jafar <mendoandev@gmail.com>
2020-12-16 21:46:58 +0700

    Add login page and function

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,199 +1,224 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import { AccountCircle, Visibility, VisibilityOff } from "@material-ui/icons";
-import { InputAdornment } from "@material-ui/core";
-import clsx from 'clsx';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
 import { userActions } from "../../redux/user.actions";
-import { connect } from 'react-redux';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://telkomathon.com/">
-        Tim Eclipse - Telkom Athon
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-  },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function LoginPage() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    password: '',
-    showPassword: false,
+import { Avatar, IconButton, InputAdornment, TextField, Typography, withStyles } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import "./login.css";
+import { compose } from "redux";
+import { deepOrange } from "@material-ui/core/colors";
+const styles = (theme) => ({
+    root: {
+        height: '100vh',
+      },
+      image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      },
+      square: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+        margin: "0 auto",
+      },
+      paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
   });
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
+    // reset login status
+    this.props.logout();
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+    this.state = {
+      username: "",
+      password: "",
+      showPassword: false,
+      submitted: false,
+    };
 
-  const onSubmit = (props)=> (event) =>{
-    event.preventDefault()
-    const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
-        } 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
+    this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
   }
 
-  
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar variant="square" className={classes.square}>
-            D
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <form className={classes.form} noValidate onSubmit={onSubmit}>
-          <TextField
-            //   variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              value={values.username}
-              onChange={handleChange('username')}
-              autoFocus
-            />
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
 
-          <TextField
-            id="outlined-adornment-password"
-            margin="normal"
-              required
-              fullWidth
-            label="Password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            fullWidth={true}
-            InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ submitted: true });
+    const { username, password } = this.state;
+    if (username && password) {
+      this.props.login(username, password);
+    }
+  }
+
+  handleClickShowPassword(e) {
+    this.setState({
+      showPassword: !this.state.showPassword,
+    });
+    console.log(this.state.showPassword);
+    // setValues({ ...values, showPassword: !values.showPassword });
+  }
+
+  handleMouseDownPassword(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    const { loggingIn } = this.props;
+    const { username, password, submitted, showPassword } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <div className="container-fluid vh-100">
+          <div
+            className="row h-100"
+            style={{ display: "flex", "flex-wrap": "wrap" }}
+          >
+            <div className="col-md-6 bg"></div>
+            <div className="col-md-6 my-auto justify-content-center">
+            <Avatar variant="square" className={classes.square}>
+                D
+              </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+              <form name="form" onSubmit={this.handleSubmit}>
+                <div
+                  className={
+                    "form-group" + (submitted && !username ? " has-error" : "")
+                  }
                 >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            labelWidth={70}
-          />
-            <FormControlLabel
-              control={<Checkbox value="rember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Log In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
+                  {/* <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={this.handleChange}
+                /> */}
+                  {submitted && !username && (
+                    <div className="help-block">Username is required</div>
+                  )}
+                  <TextField
+                    //   variant="outlined"
+                    margin="normal"
+                    required
+                    // fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    value={username}
+                    onChange={this.handleChange}
+                    autoFocus
+                  />
+                </div>
+                <div
+                  className={
+                    "form-group" + (submitted && !password ? " has-error" : "")
+                  }
+                >
+                  {/* <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value={password}
+                  onChange={this.handleChange}
+                /> */}
+                  {submitted && !password && (
+                    <div className="help-block">Password is required</div>
+                  )}
+                  <TextField
+                    margin="normal"
+                    required
+                    // fullWidth
+                    
+                    label="Password"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={password}
+                    onChange={this.handleChange}
+                    // fullWidth={true}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={this.handleClickShowPassword}
+                            onMouseDown={this.handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    // labelWidth={70}
+                  />
+                </div>
+                <div className="form-group">
+                  <button className="btn btn-primary">Login</button>
+                  {loggingIn && (
+                    <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                  )}
+                  <Link to="/register" className="btn btn-link">
+                    Register
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </Grid>
-    </Grid>
-  );
+      </div>
+    );
+  }
 }
+
 function mapState(state) {
-    const { loggingIn } = state.authentication;
-    return { loggingIn };
+  const { loggingIn } = state.authentication;
+  return { loggingIn };
 }
 
 const actionCreators = {
-    login: userActions.login,
-    logout: userActions.logout
+  login: userActions.login,
+  logout: userActions.logout,
 };
 
 const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
-export { connectedLoginPage as LoginPage };
+// export { connectedLoginPage as LoginPage };
+export default compose(
+    connect(
+      mapState,
+      actionCreators // or put null here if you do not have actions to dispatch
+    ),
+    withStyles(styles)
+  )(LoginPage);
