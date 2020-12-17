8ad6c6b3d85e253eced9792ddda176dd45a89d0e
Jafar <mendoandev@gmail.com>
2020-12-17 23:07:11 +0700

    Remove unecessary function

--- src/redux/user.actions.js
+++ src/redux/user.actions.js
@@ -8,8 +8,6 @@ export const userActions = {
     login,
     logout,
     register,
-    getAll,
-    delete: _delete
 };
 
 function login(username, password) {
@@ -64,37 +62,4 @@ function register(user) {
     function request(user) { return { type: userTypes.REGISTER_REQUEST, user } }
     function success(user) { return { type: userTypes.REGISTER_SUCCESS, user } }
     function failure(error) { return { type: userTypes.REGISTER_FAILURE, error } }
-}
-
-function getAll() {
-    return dispatch => {
-        dispatch(request());
-
-        userService.getAll()
-            .then(
-                users => dispatch(success(users)),
-                error => dispatch(failure(error.toString()))
-            );
-    };
-
-    function request() { return { type: userTypes.GETALL_REQUEST } }
-    function success(users) { return { type: userTypes.GETALL_SUCCESS, users } }
-    function failure(error) { return { type: userTypes.GETALL_FAILURE, error } }
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-    return dispatch => {
-        dispatch(request(id));
-
-        userService.delete(id)
-            .then(
-                user => dispatch(success(id)),
-                error => dispatch(failure(id, error.toString()))
-            );
-    };
-
-    function request(id) { return { type: userTypes.DELETE_REQUEST, id } }
-    function success(id) { return { type: userTypes.DELETE_SUCCESS, id } }
-    function failure(id, error) { return { type: userTypes.DELETE_FAILURE, id, error } }
 }
