4c16afc00f6a874c8601b97fa350c9b4af7a21fe
Jafar <mendoandev@gmail.com>
2020-12-19 15:00:46 +0700

    Add chatbar

---
+++ src/components/home/ChatBar.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import PeopleIcon from "@material-ui/icons/People";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
+const Accordion = withStyles({
+  root: {
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    color: "white",
+    backgroundColor: "#D4232C",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    display: "block",
+    maxHeight: 300, 
+    overflow: 'auto'
+  },
+}))(MuiAccordionDetails);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing.unit, // You might not need this now
+    position: "fixed",
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 3,
+    width: "15%",
+  },
+}));
+export default function ChatBar() {
+  const [expanded, setExpanded] = React.useState("panel1");
+  const classes = useStyles();
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        className={classes.root}
+        square
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>
+            {" "}
+            <PeopleIcon /> Friends online (5)
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {people.map((value, i) => {
+            return (
+              <List>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                  <Avatar >
+                      {value.name.charAt(0)}
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={value.name}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                        >
+                          Programmer
+                        </Typography>
+                        {/* {" — I'll be in your neighborhood doing errands this…"} */}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+              </List>
+            );
+          })}
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
+
+const people = [
+  {
+    name: `Ja'far Shodiq YB`,
+  },
+  {
+    name: "Arifin MZ",
+  },
+  {
+    name: "Rizaldy Pahlevi",
+  },
+  {
+    name: "Adit",
+  },
+  {
+    name: "Hervin Alkaff",
+  },
+];
