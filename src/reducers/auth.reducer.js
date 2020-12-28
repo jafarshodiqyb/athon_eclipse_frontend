84bfe17bd3f46f1f4822cee3757eaa12c16658cd
Jafar <mendoandev@gmail.com>
2020-12-28 15:00:31 +0700

    Temporary token expire handling

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -1,7 +1,19 @@
 import { userTypes } from './../redux/user.type';
 import jwt from 'jsonwebtoken'
 let userData = JSON.parse(localStorage.getItem('token'))
-let token =userData && userData!=null? jwt.verify(userData, '12345-67890-09876-54321'): null
+
+const token = jwt.verify(userData, '12345-67890-09876-54321', function(err, decoded) {
+  if (err) {
+    alert('Sesi habis silahkan login kembali!')
+    localStorage.removeItem('token');
+    // return ()
+  } else if (decoded) {
+    return decoded
+  
+  }
+})
+// console.log(token)
+// let token =userData && userData!=null? jwt.verify(userData, '12345-67890-09876-54321'): null
 const initialState = token ? { loggedIn: true, user : token._doc } : {}
 export function authentication(state = initialState, action) {
   switch (action.type) {
