2ecce4df93fe44648bd5d100a2c960db79494229
Jafar <mendoandev@gmail.com>
2021-01-02 22:08:40 +0700

    Add snackbar clickaway

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import { store } from "./store/configureStore";
 import { SnackbarProvider } from "notistack";
 
 // const store = store();
-
+const notistackRef = React.createRef()
 class App extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <SnackbarProvider dense={false} maxSnack={3} >
+      <SnackbarProvider dense={false} maxSnack={5}  ref={notistackRef} onClose={(event, reason, key) => {
+        if (reason === 'clickaway') {
+          notistackRef.current.closeSnackbar(key)
+        }
+      }}>
         <Provider store={store}>
           <BrowserRouter>
             <div className="App">
