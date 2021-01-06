b3f999590f5b7270040d020207fff64d6932ace1
Jafar <mendoandev@gmail.com>
2021-01-06 22:08:51 +0700

    Add hashtag reducer

--- src/store/reducer/hashtag.reducer.js
+++ src/store/reducer/hashtag.reducer.js
@@ -1,6 +1,7 @@
 import { hashtagTypes } from '../type/hashtag.type';
 import { alertTypes } from './../type/alert.type';
-
+import * as _ from "lodash"
+const hashtagRegex = require("hashtag-regex");
 export function hashtag(state = {}, action) {
   switch (action.type) {
     case hashtagTypes.HASHTAG_REQUEST:
@@ -10,7 +11,7 @@ export function hashtag(state = {}, action) {
     case hashtagTypes.HASHTAG_SUCCESS:
       return {
         // checkin: true,
-        ...state,item: action.payload
+        ...state,item: afterfound(action.payload)
       };
     case hashtagTypes.HASHTAG_FAILURE:
       return {};
@@ -19,4 +20,30 @@ export function hashtag(state = {}, action) {
     default:
       return state
   }
+}
+
+function afterfound(posts) {
+    const regex = hashtagRegex();
+    let match;
+    let find = [];
+    // return new Promise((resolve,reject)=>{
+        if(posts){
+            posts.map((value, i) => {
+                while ((match = regex.exec(value.posts.content))) {
+                  const hashtag = match[0];
+                  let index = _.findIndex(find,function(o) { return o.hashtag == hashtag; })
+                  console.log(index)
+                  if (index !== -1 ) {
+                    find[index].count = find[index].count + 1;
+                    // find[hashtag].count = find[hashtag].count++;
+                  } else {
+                    find.push({
+                      hashtag: hashtag,
+                      count: 1})
+                    };
+                  }
+                });
+        }
+          return(find)
+    // })
 }
